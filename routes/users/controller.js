e7f9688a6ad36f32e3d4fa31a22c4210a03f562c
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-07-14 12:04:31 +0700

    Create get all users route and get user profile route

--- routes/users/controller.js
+++ routes/users/controller.js
@@ -1,6 +1,27 @@
 const { User } = require('../../models');
 
 module.exports = {
+    getAllUsers: async (req, res) => {
+        try {
+            const results = await User.findAll();
+
+            res.send({
+                result,
+            });
+        } catch (error) {
+            res.send(error);
+        }
+    },
+    getUserProfile: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const result = await User.findByPk(id);
+
+            res.send(result);
+        } catch (error) {
+            res.send(error);
+        }
+    },
     userRegistration: async (req, res) => {
         const { fullName, email, password } = req.body;
 
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { userRegistration } = require('./controller');
+const {
+    userRegistration,
+    getAllUsers,
+    getUserProfile,
+} = require('./controller');
 
+router.get('/', getAllUsers);
+router.get('/:id', getUserProfile);
 router.post('/', userRegistration);
 
 module.exports = router;
